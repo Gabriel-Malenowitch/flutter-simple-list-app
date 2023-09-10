bca18fb0d3bc197b84c49af6f6181ae1910f0ac7
Gabriel-Malenowitch <gabrielbotelhomalenowitch@gmail.com>
2023-09-10 17:07:53 -0300

    feat: create list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
+// Deixando aqui só pra ficar mais fácil de ler, mas essa constante deveria estar em outro lugar.
+final globalWords = [
+  'Bananaquedabra',
+  'Almofarizinhos',
+  'Pamonhazinha',
+  'Cocoricólogos',
+  'Xaropambulante',
+  'Fofocandopatia',
+  'Cachorroquente',
+  'Tchutchucar',
+  'Breguicinha',
+  'Mingauzada',
+  'Palhaçadastrófico',
+  'Chocalhotas',
+  'Babadoconfusão',
+  'Trapalhiquices',
+  'Boboquicestral',
+  'Malabarinhas',
+  'Gravatacolorida',
+  'Cacarejante',
+];
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,15 +30,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.deepPurple[400],
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My first app!!'),
-        ),
-      ),
-    );
+        title: 'Flutter Demo',
+        theme:
+            ThemeData(appBarTheme: const AppBarTheme(color: Colors.deepPurple)),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('My first app!!'),
+            centerTitle: true,
+            // backgroundColor: Colors.deepPurple,
+          ),
+          body: BodyWidgetWithState(),
+        ));
+  }
+}
+
+class BodyWidgetWithState extends StatefulWidget {
+  final List<String> words = globalWords;
+  final List<String> savedWords = [];
+
+  BodyWidgetWithState({super.key});
+
+  @override
+  Body createState() => Body();
+}
+
+class Body extends State<BodyWidgetWithState> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        itemCount: widget.words.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          final word = widget.words[index];
+          final isAlreadySaved = widget.savedWords.contains(word);
+
+          return ListTile(
+              trailing: Icon(
+                  isAlreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: isAlreadySaved ? Colors.deepPurple : null),
+              title: Text(widget.words[index]),
+              onTap: () {
+                setState(() {
+                  if (isAlreadySaved) {
+                    widget.savedWords.remove(word);
+                  } else {
+                    widget.savedWords.add(word);
+                  }
+                });
+              });
+        });
   }
 }
